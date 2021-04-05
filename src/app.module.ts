270f52497bbfa626f8dedfbf2acecdc005b7bcc1
Mohammad Safia <msafia@terkwaz.com>
2021-04-06 01:06:12 +0300

    Connect app with mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
+
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://MohammadSafia:TAfMtUnAQiFoHJzQ@cluster0.t5cfl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
